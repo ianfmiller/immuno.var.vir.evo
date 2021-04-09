cb79f1688ecf7f7bd16e0740b3d0f197bb8f3ba6
ifmiller <ifmiller@princeton.edu>
2021-04-09 12:13:42 -0400

    script to run entire analysis

---
+++ cluster/run_analysis.R
@@ -0,0 +1,18 @@
+rm(list=ls())
+
+# parameter sets to explore
+params.mat<-data.frame("p.immune"=c(1,1,1,1,1),"w"=c(1,0,0,0,0),"x"=c(0,1,0,0,.5),"y"=c(0,0,1,0,0),"z"=c(0,1,0,1,1))
+alphas<-10^(seq(-2,2,.25))
+
+for(i in 1:dim(params.mat)[1])
+{
+  p.immune<-params.mat[i,"p.immune"]
+  w<-params.mat[i,"w"]
+  x<-params.mat[i,"w"]
+  y<-params.mat[i,"w"]
+  z<-params.mat[i,"w"]
+  
+  source("~/immuno.var.vir.evo/code/folder.setup.R")
+  setwd(new.dir)
+  system("sbatch batch.script.q")
+}
