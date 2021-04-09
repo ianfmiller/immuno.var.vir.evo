19131618fdb2c51c0dd8cf663b84a8e28f24e0eb
ifmiller <ifmiller@princeton.edu>
2021-04-09 13:51:21 -0400

    file name update

--- cluster/code/data.compilation.R
+++ cluster/code/data.compilation.R
@@ -3,17 +3,16 @@ w<-0
 x<-0.5
 y<-0
 z<-0
-p.vacc<-1
+p.immune<-1
 alpha<--1
 
 out.data<-c()
-      
+setwd("~/immuno.var.vir.evo/output")
+
 for (i in 1:19)
 {
-  setwd(paste("~/hetero/",paste("p",p.vacc,"W",w,"X",x,"Y",y,"Z",z,sep=""),"/dir.",i,sep=""))
-  data<-readRDS(dir()[grep(".Rds",dir())])
+  data<-readRDS(paste("p",p.immune,"W",w,"X",x,"Y",y,"Z",z,"alpha",alpha,".RDS",sep=""))
   out.data<-rbind(out.data,data)
 }
 
-setwd(paste("~/hetero/",paste("p",p.vacc,"W",w,"X",x,"Y",y,"Z",z,sep=""),sep=""))
-write.csv(out.data,file=paste("p",p.vacc,"W",w,"X",x,"Y",y,"Z",z,".csv",sep=""))
+write.csv(out.data,file=paste("p",p.immune,"W",w,"X",x,"Y",y,"Z",z,".csv",sep=""))
