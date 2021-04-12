05b8350dd15c000e6181ff6d3e637e2560322618
ifmiller <ifmiller@princeton.edu>
2021-04-12 18:57:17 -0400

    switch back to tried and true ess.analysis func

--- cluster/code/analysis.setup.R
+++ cluster/code/analysis.setup.R
@@ -273,7 +273,8 @@ plot.startconds.2<-function(ymax=1,col="black",main)
 ### analysis of ESS
 ess.analysis<-function(mat)
 {
-  output<-rep(NA,length.out=3)
+  output<-rep(NA,length.out=13)
+  if(colnames(mat)[1]=="X") {mat<-mat[,-1]}
   mod.mat<-matrix(NA,dim(mat)[1],dim(mat)[2])
   colnames(mod.mat)<-colnames(mat)
   rownames(mod.mat)<-rownames(mat)
@@ -288,12 +289,110 @@ ess.analysis<-function(mat)
   col.max<-find.peak(colSums(mod.mat))
   col.min<-find.peak(-1*colSums(mod.mat))
   
-  if (is.numeric(col.min)&&is.numeric(row.max)) {if(col.min-row.max<=1) {output[1]<-"ESS"; output[2]<-virulence.steps[col.min]}}
+  if (is.numeric(col.max)&&is.numeric(row.min)) {if(col.max-row.min<=1) {output[1]<-"ESS"; output[2]<-virulence.steps[col.max]}}
+  if (is.numeric(col.min)&&is.numeric(row.max)) {if(col.min-row.max<=1) {output[3]<-"REPELLER"; output[4]<-virulence.steps[row.max]}}
+  
+  if(isTRUE(as.numeric(output[2])>0)) #used to check for repeller point and get eradication bounds
+  {
+    up.sub.mat.cols<-which(virulence.steps>max(as.numeric(output[2],output[4])))
+    up.sub.mat<-mod.mat[up.sub.mat.cols,up.sub.mat.cols]
+    up.sub.mat.col.sums<-colSums(up.sub.mat)
+    
+    if (length(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))>=2)
+    {
+      upper.bound<-max(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))+(dim(mod.mat)[1]-length(up.sub.mat.col.sums))
+      lower.bound<-min(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))+(dim(mod.mat)[1]-length(up.sub.mat.col.sums))
+      if(upper.bound<dim(mat)[1] && is.na(output[3]))
+      {
+        output[3]<-"REPELLER2"
+        output[4]<-virulence.steps[upper.bound+1]
+        output[10]<-"mid.erad.lower"
+        output[11]<-virulence.steps[lower.bound]
+        output[12]<-"mid.erad.upper"
+        output[13]<-virulence.steps[upper.bound]
+        
+      }
+      
+      if(upper.bound==dim(mat)[1])
+      {
+        output[6]<-"upper.erad"
+        output[7]<-virulence.steps[lower.bound]
+      }
+    }
+    
+    low.sub.mat.cols<-which(virulence.steps<output[2])
+    low.sub.mat<-mod.mat[low.sub.mat.cols,low.sub.mat.cols]
+    low.sub.mat.col.sums<-colSums(low.sub.mat)
+    
+    if (length(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))>=2)
+    {
+      upper.bound<-max(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))
+      lower.bound<-min(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))
+      
+      if(lower.bound==1)
+      {
+        output[8]<-"lower.erad"
+        output[9]<-virulence.steps[upper.bound]
+      }
+    }
+  }
   
   
   if (isFALSE(any(diff(colSums(mod.mat))<0)) && any(mod.mat>0)) 
   {
-    output[4]<-"selection for hypervirulence"
+    output[5]<-"selection for hypervirulence"
+    if(length(which(colSums(mod.mat)==min(colSums(mod.mat))))>2)
+    {
+      output[8]<-"lower.erad"
+      output[9]<-virulence.steps[max(which(colSums(mod.mat)==min(colSums(mod.mat))))]
+    }
+  }
+  
+  
+  if (isFALSE(any(diff(colSums(mod.mat))>0)) && any(mod.mat>0)) {output[5]<-"selection for 0 virulence"}
+  
+  if (is.na(output[5]) && is.na(col.max) && is.na(col.min) && is.na(row.max) && is.na(row.min)) {output[5]<-"global eradication"}
+  
+  if (!any(is.na(output)==F)) #this loop is for when theres a region around the ESS where nothing can invade. This is due to numerical errors
+  {
+    intersect(which(colSums(mod.mat)==max(colSums(mod.mat))),which(rowSums(mod.mat)==min(rowSums(mod.mat))))->overlap
+    if (length(overlap)>0)
+    {
+      output[1]<-"ESS"
+      output[2]<-mean(virulence.steps[overlap])
+      { #get erad bounds
+        up.sub.mat.cols<-which(virulence.steps>max(as.numeric(output[2],output[4])))
+        up.sub.mat<-mod.mat[up.sub.mat.cols,up.sub.mat.cols]
+        up.sub.mat.col.sums<-colSums(up.sub.mat)
+        
+        if (length(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))>=2)
+        {
+          upper.bound<-max(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))+(dim(mod.mat)[1]-length(up.sub.mat.col.sums))
+          lower.bound<-min(which(up.sub.mat.col.sums==min(up.sub.mat.col.sums)))+(dim(mod.mat)[1]-length(up.sub.mat.col.sums))
+          if(upper.bound==dim(mat)[1])
+          {
+            output[6]<-"upper.erad"
+            output[7]<-virulence.steps[lower.bound]
+          }
+        }
+        
+        low.sub.mat.cols<-which(virulence.steps<output[2])
+        low.sub.mat<-mod.mat[low.sub.mat.cols,low.sub.mat.cols]
+        low.sub.mat.col.sums<-colSums(low.sub.mat)
+        
+        if (length(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))>=2)
+        {
+          upper.bound<-max(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))
+          lower.bound<-min(which(low.sub.mat.col.sums==min(low.sub.mat.col.sums)))
+          
+          if(lower.bound==1)
+          {
+            output[8]<-"lower.erad"
+            output[9]<-virulence.steps[upper.bound]
+          }
+        }
+      }
+    }
   }
   
   return(output)
