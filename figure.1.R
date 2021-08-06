a055eefe191e142150e81081596fd2c093030f80
ifmiller <ifmiller@princeton.edu>
2021-08-06 09:22:15 -0400

    update plotting

--- figure.1.R
+++ figure.1.R
@@ -21,7 +21,8 @@ par(mar=c(4,4,3,1))
 set.immunity.dist.split()
 initial.infection.structure<-function(x) {0.0*scaled.immunity.distribution(x)}
 get.startconds()
-plot.startconds.2(ymax=.5,col="black",main="split")
+plot.startconds.2(ymax=.5,col="black",main="polarized")
+mtext("A",3,adj=1,font=2)
 
 for (alpha in alphas)
 {
@@ -29,19 +30,23 @@ for (alpha in alphas)
   set.immunity.dist.beta(alpha,beta)
   initial.infection.structure<-function(x) {0.0*scaled.immunity.distribution(x)}
   get.startconds()
-  plot.startconds.2(ymax=.5,col=plot.colors[which(alphas==alpha)],main=substitute(paste(alpha," = ",beta," = ",10^z),list(z=log10(alpha))))
+  plot.startconds.2(ymax=.5,col=plot.colors[which(alphas==alpha)],main=substitute(paste(theta," = ",10^z),list(z=log10(alpha))))
+  mtext(c("B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R")[which(alphas==alpha)],3,adj=1,font=2)
 }
 
 set.immunity.dist.single()
 initial.infection.structure<-function(x) {0.0*scaled.immunity.distribution(x)}
 get.startconds()
 plot.startconds.2(ymax=1,col="darkgrey",main="homogeneous")
+mtext("S",3,adj=1,font=2)
 
-par(mar=c(6,5,3,5))
+
+par(mar=c(7,5,3,5))
 plot(0,0,xlim=c(-2.25,2.25),ylim=c(0,1),xlab=expression(log[10](alpha)),ylab="mean",type="n",axes = F,cex.lab=2)
+mtext("T",3,adj=1,font=2)
 box()
 axis(1,at=c(-2,-1,0,1,2),labels=c(-2,-1,0,1,2),cex.axis=1.5)
-axis(1,at=c(-2.25,2.25),labels=c("split","homog."),las=2,cex.axis=1.5)
+axis(1,at=c(-2.25,2.25),labels=c("polarized","homog."),las=2,cex.axis=1.5)
 axis(2,cex.axis=1.5)
 abline(v=-2.125,col="grey",lty=2,lwd=2.5)
 abline(v=2.125,col="grey",lty=2,lwd=2.5)
@@ -68,11 +73,12 @@ get.startconds()
 yy<-weighted.mean(immunity.categories,scaled.immunity.distribution(dummy.immunity.categories))
 points(2.25,yy,col="darkgrey",pch=16,cex=4)
 
-par(mar=c(6,5,3,5))
+par(mar=c(7,5,3,5))
 plot(0,0,xlim=c(-2.25,2.25),ylim=c(-.05,.3),xlab=expression(log[10](alpha)),ylab="variance",type="n",axes = F,cex.lab=2)
+mtext("U",3,adj=1,font=2)
 box()
 axis(1,at=c(-2,-1,0,1,2),labels=c(-2,-1,0,1,2),cex.axis=1.5)
-axis(1,at=c(-2.25,2.25),labels=c("split","homog."),las=2,cex.axis=1.5)
+axis(1,at=c(-2.25,2.25),labels=c("polarized","homog."),las=2,cex.axis=1.5)
 axis(2,cex.axis=1.5)
 abline(v=-2.125,col="grey",lty=2,lwd=2.5)
 abline(v=2.125,col="grey",lty=2,lwd=2.5)
